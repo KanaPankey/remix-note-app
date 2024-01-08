de37835ca13b21d82a9ef4d7c95d85eecc37c19c
KanaPankey <86436056+KanaPankey@users.noreply.github.com>
2024-01-08 10:55:05 -0600

    added action and loader

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,20 +1,38 @@
 import { ActionFunctionArgs } from '@remix-run/node';
 import { redirect } from 'react-router';
+import NoteList, { links as noteListLinks } from '~/components/NoteList';
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
 import { getStoredNotes, storeNotes } from '~/data/notes';
+import { useLoaderData } from '@remix-run/react';
 
 
 export default function NotesPage() {
+  const notes = useLoaderData();
+
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
-  )
+  );
+}
+
+export async function loader() {
+  const notes = await getStoredNotes();
+  return notes;
 }
 
 export async function action ({request}: ActionFunctionArgs) {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData);
+
+  // TODO:  Find a better way to type narrow to string...some sort of parse?
+  if (typeof noteData.title == 'string') {
+    if (noteData.title.trim().length < 5) {
+      return {message: 'Invalid title - must be at least 5 characters long.'};
+    }
+  }
+
 
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
@@ -26,5 +44,5 @@ export async function action ({request}: ActionFunctionArgs) {
 }
 
 export function links() {
-  return [...newNoteLinks()];
+  return [...newNoteLinks(), ...noteListLinks()];
 }
