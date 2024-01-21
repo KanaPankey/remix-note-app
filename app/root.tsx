dc0536fc2228ab843231c19b94efbb3baeecc95f
KanaPankey <86436056+KanaPankey@users.noreply.github.com>
2024-01-21 12:38:18 -0600

    sets error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 // import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
+
 import styles from '~/styles/main.css';
 import MainNavigation from '~/components/MainNavigation';
 import {
@@ -10,6 +11,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
@@ -39,30 +42,61 @@ export default function App() {
   );
 }
 
-export function ErrorBoundary({error: }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-        <title>An error occurred!</title>
-      </head>
-      <body>
-        <header>
-          <MainNavigation />
-        </header>
-        <main>
-          <h1>An error occurred!</h1>
-          <p>{error.message}</p>
-          <p>Back to <Link to="/">safety</Link>!</p>
-        </main>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  
+  if (isRouteErrorResponse(error)) {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+          <title>An error occurred!</title>
+        </head>
+        <body>
+          <header>
+            <MainNavigation />
+          </header>
+          <main className='error'>
+            <h1>An error occurred!</h1>
+            <p>This error {error.data}</p>
+            <p>Back to <Link to="/">safety</Link>!</p>
+          </main>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  } else if (error instanceof Error) {
+    return(
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+          <title>An error occurred!</title>
+        </head>
+        <body>
+          <header>
+            <MainNavigation />
+          </header>
+          <main className='error'>
+            <h1>An error occurred!</h1>
+            <p>{error.message}</p>
+            <p>Back to <Link to="/">safety</Link>!</p>
+          </main>
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    ) 
+  } else {
+    return (<h1>Unknown Error</h1>)
+  }
 }
