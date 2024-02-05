32662807a741afed933dd37c2c604a8483cdaf5c
KanaPankey <86436056+KanaPankey@users.noreply.github.com>
2024-02-05 08:27:23 -0600

    adds info in detail notes page

--- app/routes/notes_.$noteId.tsx
+++ app/routes/notes_.$noteId.tsx
@@ -1,8 +1,9 @@
-import { Link } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
+import { getStoredNotes } from '~/data/notes';
 import styles from '~/styles/note-details.css';
 
 export default function NoteDetailsPage() {
-  // const note = useLoaderData();
+  const note: NoteType = useLoaderData();
   return (
     <main id="note-details">
       {/* <Note note={note} /> */}
@@ -10,13 +11,26 @@ export default function NoteDetailsPage() {
         <nav>
           <Link to={"/notes"}>Back to all notes</Link>
         </nav>
-        <h1>NOTE TITLE</h1>
+        <h1>{note.title}</h1>
       </header>
-      <p id="note-details-content">NOTE CONTENT</p>
+      <p id="note-details-content">{note.content}</p>
     </main>
   );
+}
+
+export async function loader(data: {params: {noteId: string}}) {
+  const notes = await getStoredNotes();
+  const selectedNote = notes.find((note: NoteType) => note.id === data.params.noteId);
+
+  if (!selectedNote) {
+    throw new Error('Note not found: ' + data.params.noteId);
+  }
+
+  return selectedNote;
 }
 
 export function links() {
   return [{rel: 'stylesheet', href: styles}];
 }
+
+type NoteType = {id: string, title: string, content: string};
